edae91409afa7d46ff9f33a185cbbc0bf089c649
Toby Betts <toby.betts@sky.uk>
2022-05-06 00:18:49 +0100

    styling changes

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, {useState, useRef} from 'react';
 import {Text, View, Dimensions, Image} from 'react-native';
-import Carousel from 'react-native-snap-carousel';
+import Carousel, {Pagination} from 'react-native-snap-carousel';
+
 
 import data from './data';
 
@@ -26,15 +27,41 @@ const renderItem = ({item}) => {
 };
 
 const App = () => {
+  const [index, setIndex] = useState(0);
+  const isCarousel = useRef(null);
   return (
-    <View style={{marginVertical: 10}}>
+    <View style={{ backgroundColor: 'purple', height: 'auto', flex: 1, justifyContent: 'flex-end' }}>
+    <View style={{marginVertical: 10 }}>
       <Carousel
+        ref={isCarousel}
         data={data}
         renderItem={renderItem}
         sliderWidth={SLIDER_WIDTH}
         itemWidth={ITEM_WIDTH}
+        onSnapToItem={index => setIndex(index)}
       />
+      <Pagination
+        dotsLength={data.length}
+        activeDotIndex={index}
+        carouselRef={isCarousel}
+        dotStyle={{
+          width: 15,
+          height: 15,
+          borderRadius: 5,
+          marginHorizontal: 8,
+          backgroundColor: 'orange',
+        }}
+        tappableDots={true}
+        inactiveDotStyle={{
+          backgroundColor: 'black',
+          // Define styles for inactive dots here
+        }}
+        inactiveDotOpacity={0.4}
+        inactiveDotScale={0.6}
+      />
+    </View>
     </View>
   );
 };
+
 export default App;
