882ac3af00af5cbd53602c3e94e8d6a5184f4df6
Toby Betts <toby.betts@sky.uk>
2022-05-05 23:57:06 +0100

    carousel idea

--- App.js
+++ App.js
@@ -1,20 +1,40 @@
-import React from 'react'
-import { SafeAreaView, StyleSheet } from 'react-native'
-import CarouselCards from './CarouselCards'
+import React from 'react';
+import {Text, View, Dimensions, Image} from 'react-native';
+import Carousel from 'react-native-snap-carousel';
 
-export default function App() {
+import data from './data';
+
+export const SLIDER_WIDTH = Dimensions.get('window').width + 30;
+export const ITEM_WIDTH = Math.round(SLIDER_WIDTH * 0.8);
+
+const renderItem = ({item}) => {
   return (
-    <SafeAreaView style={styles.container}>
-      <CarouselCards />
-    </SafeAreaView>
+    <View
+      style={{
+        borderWidth: 1,
+        padding: 20,
+        borderRadius: 20,
+        alignItems: 'center',
+        backgroundColor: 'white',
+      }}>
+      <Image source={{uri: item.url}} style={{width: 200, height: 200}} />
+      <Text style={{marginVertical: 10, fontSize: 20, fontWeight: 'bold'}}>
+        {item.name}
+      </Text>
+    </View>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-    padding: 50
-  },
-});
+const App = () => {
+  return (
+    <View style={{marginVertical: 10}}>
+      <Carousel
+        data={data}
+        renderItem={renderItem}
+        sliderWidth={SLIDER_WIDTH}
+        itemWidth={ITEM_WIDTH}
+      />
+    </View>
+  );
+};
+export default App;
--- data.js
+++ data.js
@@ -1,19 +1,19 @@
 const data = [
     {
-      title: "Aenean leo",
-      body: "Ut tincidunt tincidunt erat. Sed cursus turpis vitae tortor. Quisque malesuada placerat nisl. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem.",
-      imgUrl: "https://picsum.photos/id/11/200/300",
+      id: 1,
+      name: 'React JS',
+      url: 'https://icon-library.com/images/react-icon/react-icon-29.jpg',
     },
     {
-      title: "In turpis",
-      body: "Aenean ut eros et nisl sagittis vestibulum. Donec posuere vulputate arcu. Proin faucibus arcu quis ante. Curabitur at lacus ac velit ornare lobortis. ",
-      imgUrl: "https://picsum.photos/id/10/200/300",
+      id: 2,
+      name: 'JavaScript',
+      url: 'https://upload.wikimedia.org/wikipedia/commons/3/3b/Javascript_Logo.png',
     },
     {
-      title: "Lorem Ipsum",
-      body: "Phasellus ullamcorper ipsum rutrum nunc. Nullam quis ante. Etiam ultricies nisi vel augue. Aenean tellus metus, bibendum sed, posuere ac, mattis non, nunc.",
-      imgUrl: "https://picsum.photos/id/12/200/300",
+      id: 3,
+      name: 'Node JS',
+      url: 'https://upload.wikimedia.org/wikipedia/commons/6/67/NodeJS.png',
     },
   ];
-  
-  export default data;
+
+  export default data
